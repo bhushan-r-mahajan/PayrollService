8662603935dceb156b84f18115a0f61e7b13d11f
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-05-28 14:43:57 +0530

    Implemented prepared statement and refactored

--- src/main/java/PayrollService.java
+++ src/main/java/PayrollService.java
@@ -4,6 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PayrollService {
+    List<EmployeeData> employeeDataList = new ArrayList<>();
+
+    private PreparedStatement employeePayrollDataStatement;
+    private static PayrollService instance;
+
+    public static PayrollService getInstance() {
+        if (instance == null) {
+            instance = new PayrollService();
+        }
+        return instance;
+    }
 
     private Connection getConnection() throws CustomException {
         String jdbcUrl = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
@@ -11,22 +22,16 @@ public class PayrollService {
         String password = "Bhushan@0611";
         Connection connection;
         try {
-            System.out.println("Connecting to database " + jdbcUrl);
             connection = DriverManager.getConnection(jdbcUrl, userName, password);
-            System.out.println("Connected to " + connection + " Successful!!!!");
         } catch (SQLException e) {
             throw new CustomException("Connection to Database Failed!!");
         }
         return connection;
     }
 
-    public List<EmployeeData> readDataFromDB() throws CustomException {
-        String sql = "select * from employee_payroll;";
+    private List<EmployeeData> getDataFromDB(ResultSet result) throws CustomException {
         List<EmployeeData> employeeData = new ArrayList<>();
-
-        try (Connection connection = this.getConnection()) {
-            Statement statement = connection.createStatement();
-            ResultSet result = statement.executeQuery(sql);
+        try {
             while (result.next()) {
                 int id = result.getInt("id");
                 String name = result.getString("name");
@@ -35,8 +40,54 @@ public class PayrollService {
                 employeeData.add(new EmployeeData(id, name, salary, start_date));
             }
         } catch (SQLException e) {
-            throw new CustomException("Failed");
+            throw new CustomException("Failed!!");
         }
         return employeeData;
+    }
+
+    public List<EmployeeData> readAllDataFromTable() throws CustomException {
+        String sql = "select * from employee_payroll;";
+        try (Connection connection = this.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+            employeeDataList = getDataFromDB(result);
+        } catch (SQLException e) {
+            throw new CustomException("Failed");
+        }
+        return employeeDataList;
+    }
+
+    public List<EmployeeData> getEmployeePayrollData(String fname) throws CustomException {
+        if(this.employeePayrollDataStatement == null){
+            this.preparedStatementForEmployeeData();
+        }
+        try {
+            employeePayrollDataStatement.setString(1, fname);
+            ResultSet result = employeePayrollDataStatement.executeQuery();
+            employeeDataList = getDataFromDB(result);
+        }catch (SQLException e) {
+            throw new CustomException("Failed!!");
+        }
+        return employeeDataList;
+    }
+
+    private void preparedStatementForEmployeeData() throws CustomException {
+        try {
+            Connection connection = this.getConnection();
+            String sql = "select * from employee_payroll where name = ?";
+            employeePayrollDataStatement = connection.prepareStatement(sql);
+        } catch (SQLException e) {
+            throw new CustomException("Prepared Statement Failed!");
+        }
+    }
+
+    public int updateDataInDB(String name, double salary) throws CustomException {
+        String sql = String.format("update employee_payroll set salary = %.2f where name = '%s';", salary, name);
+        try (Connection connection = this.getConnection()) {
+            Statement statement = connection.createStatement();
+            return statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            throw new CustomException("Update Failed!!!");
+        }
     }
 }
--- src/test/java/PayrollServiceTest.java
+++ src/test/java/PayrollServiceTest.java
@@ -1,14 +1,33 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PayrollServiceTest {
+    List <EmployeeData> employeeData = new ArrayList<>();
+
     @Test
     void giveDataFromADatabase_WhenRetrived_ShouldMatchTheCount() throws CustomException {
-        PayrollService payrollService = new PayrollService();
-        List <EmployeeData> employeeData = payrollService.readDataFromDB();
+        employeeData = PayrollService.getInstance().readAllDataFromTable();
         System.out.println("The Number of Entries in DataBase are = " + employeeData.size());
         Assertions.assertEquals(3, employeeData.size());
+    }
+
+    @Test
+    void givenNewSalary_WhenUpdated_ShouldPassTest() throws CustomException {
+        employeeData = PayrollService.getInstance().readAllDataFromTable();
+        int result = PayrollService.getInstance().updateDataInDB("shashank", 3000000);
+        System.out.println(employeeData.get(0));
+        System.out.println(PayrollService.getInstance().getEmployeePayrollData("Shashank"));
+        Assertions.assertEquals(1, result);
+    }
+
+    @Test
+    void givenNewSalary_WhenUpdated_ShouldSyncWithDB() throws CustomException {
+        employeeData = PayrollService.getInstance().readAllDataFromTable();
+        PayrollService.getInstance().updateDataInDB("shashank", 3000000);
+        boolean result = employeeData.get(0).equals(PayrollService.getInstance().getEmployeePayrollData("Shashank"));
+        Assertions.assertTrue(result);
     }
 }
